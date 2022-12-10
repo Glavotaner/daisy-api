a6a546f08cd0e3005486ce2141512723a18eb321
marin <marin.glavas@outlook.com>
2022-12-10 13:15:15 +0100

    refactor callback logic

--- src/repositories/user/user.repository.ts
+++ src/repositories/user/user.repository.ts
@@ -2,17 +2,9 @@ import { RegistrationData } from "../../services/user/user.service";
 
 export abstract class UserRepository {
     abstract get({ username }: { username: string }): Promise<User | undefined>;
-    // TODO don't use callbacks
-    async tryGetUser(username: string, { onFound, onNotFound }: { onFound: userFoundCallback, onNotFound: userNotFoundCallback }): Promise<any> {
-        const user = await this.get({ username });
-        return user ? onFound(user) : onNotFound();
-    }
     abstract create({ username, token }: RegistrationData): Promise<any>;
     abstract update(username: string, properties: Record<string, any>): Promise<any>;
 }
-
-type userFoundCallback = (user: User) => Promise<any>;
-type userNotFoundCallback = () => Promise<any>;
 
 export interface User {
     username: string;
