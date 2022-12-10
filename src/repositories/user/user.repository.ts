1b149b36f37303b002e22b65ed17f1a25c726d12
marin <marin.glavas@outlook.com>
2022-12-10 12:08:18 +0100

    implement interface

--- src/repositories/user/user.repository.ts
+++ src/repositories/user/user.repository.ts
@@ -1,10 +1,13 @@
+import { RegistrationData } from "../../services/user/user.service";
+
 export abstract class UserRepository {
     abstract get({ username }: { username: string }): Promise<User | undefined>;
+    // TODO don't use callbacks
     async tryGetUser(username: string, { onFound, onNotFound }: { onFound: userFoundCallback, onNotFound: userNotFoundCallback }): Promise<any> {
         const user = await this.get({ username });
         return user ? onFound(user) : onNotFound();
     }
-    abstract create({ username, token }: { username: string, token: string }): Promise<any>;
+    abstract create({ username, token }: RegistrationData): Promise<any>;
     abstract update(username: string, properties: Record<string, any>): Promise<any>;
 }
 
